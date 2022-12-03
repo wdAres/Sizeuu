2ab67ea7e718c1e68a9ae7f7ce130aa037fe4b51
wdAres <puneetsri9990@gmail.com>
2022-12-03 13:50:36 +0530

    .

---
+++ javascript/index.js
@@ -0,0 +1,29 @@
+$("#partners-container").slick({
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  slidesToShow: 5,
+  centerMode: true,
+  centerPadding: '60px'
+  // responsive: [
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 3
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 1
+  //     }
+  //   }
+  // ]
+});
